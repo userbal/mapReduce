59b92902eda1a4815667c07685c8405d6178a68b
Brigg Lewis <d00256609@dmail.dixie.edu>
2019-04-02 21:22:56 -0600

    almost finished part1

--- main.go
+++ main.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"database/sql"
+	"io"
 	"log"
+	"math"
+	"net/http"
 	"os"
-	"time"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// dbError is an error implementation that includes a time and message.
-type dbError struct {
-	When time.Time
-	What string
-}
-
 func openDatabase(path string) (*sql.DB, error) {
-	dbError := new(dbError)
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		log.Fatal(err)
-		dbError.What = "died while opening"
-		return db, errcode
 	}
-	defer db.Close()
+	//defer db.Close()
 
 	sqlStmt := `
 	pragma synchronous = off;
@@ -31,22 +25,20 @@ func openDatabase(path string) (*sql.DB, error) {
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
-		dbError.What = ("died while running " + sqlStmt)
 		return db, err
 	}
 
-	sqlStmt := `
+	sqlStmt = `
 	pragma journal_mode = off;
 	`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
-		dbError.What = ("died while running %v", sqlStmt)
 		return db, err
 	}
 
-	dbError.What = ("")
-	return db, err
+	return db, nil
+
 }
 
 func createDatabase(path string) (*sql.DB, error) {
@@ -58,12 +50,183 @@ func createDatabase(path string) (*sql.DB, error) {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	sqlStmt := `
+	pragma synchronous = off;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return db, err
+	}
+
+	sqlStmt = `
+	pragma journal_mode = off;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return db, err
+	}
+
+	sqlStmt = `
+	create table pairs (key text, value text);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return db, err
+	}
+
+	return db, nil
 }
 
 func splitDatabase(source, outputPattern string, m int) ([]string, error) {
-	// example call
+	// example call:
 	//paths, err := splitDatabase("input.sqlite3", "output-%d.sqlite3", 50)
 
+	//creating databases
+	var partitionNames []string
+	for i := 0; i <= m; i++ {
+		iS := strconv.Itoa(i)
+		x := Sprintf(outputPattern, iS)
+		_, err := createDatabase(x)
+		if err != nil {
+			log.Fatal(err)
+		}
+		partitionNames = append(partitionNames, x)
+	}
+
+	//opening source
+	db, err := openDatabase(source)
+
+	//figure out how many pairs should be in each partition
+	stmt, err := db.Prepare("select count(1) from pairs")
+	if err != nil {
+		log.Println(err)
+		return partitionNames, err
+	}
+	defer stmt.Close()
+	var nPairs int
+	err = stmt.QueryRow().Scan(&nPairs)
+	if err != nil {
+		log.Println(err)
+		return partitionNames, err
+	}
+
+	if nPairs < m {
+		log.Fatal("you must request fewer partitions than rows, (split database)")
+	}
+	x := nPairs / m
+	y := float64(x)
+	y = math.Floor(y)
+	rowsPerPartition := int(y)
+
+	//insert rows into partitions
+	rows, err := db.Query("select key, value from pairs")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var key string
+	var value string
+	i := 0
+	a := 0
+	for rows.Next() {
+		if a >= rowsPerPartition && i < m {
+			i++
+			a = 0
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		db, err := openDatabase(partitionNames[i])
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = tx.Exec("insert into pairs(key, value) values(?, ?)", key, value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tx.Commit()
+		db.Close()
+	}
+
+	return partitionNames, nil
 }
 
-func main() {}
+func mergeDatabases(urls []string, path string, temp string) (*sql.DB, error) {
+	db, err := createDatabase(path)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	for _, url := range urls {
+		download(url, temp)
+		gatherInto(db, temp)
+		os.Remove(temp)
+	}
+
+	return db, nil
+}
+
+func download(url, path string) error {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	tempFile, err := os.Create(path)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	_, err = io.Copy(tempFile, res.Body)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	res.Body.Close()
+	tempFile.Close()
+	return nil
+}
+
+func gatherInto(db *sql.DB, path string) error {
+	sqlStmt := `
+	attach ? as merge
+	`
+	_, err := db.Exec(sqlStmt, path)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return err
+	}
+
+	sqlStmt = `
+	insert into pairs select * from merge.pairs
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return err
+	}
+
+	sqlStmt = `
+	detach merge
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return err
+	}
+
+	os.Remove(path)
+	return nil
+}
+
+func main() {
+	splitDatabase("austen.sqlite3", "output-%d.sqlite3", 5)
+
+}
---
+++ worker.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type MapTask struct {
+	M, R       int    // total number of map and reduce tasks
+	N          int    // map task number, 0-based
+	SourceHost string // address of host with map input file
+}
+
+type ReduceTask struct {
+	M, R        int      // total number of map and reduce tasks
+	N           int      // reduce task number, 0-based
+	SourceHosts []string // addresses of map workers
+}
+
+type Pair struct {
+	Key   string
+	Value string
+}
+
+type Interface interface {
+	Map(key, value string, output chan<- Pair) error
+	Reduce(key string, values <-chan string, output chan<- Pair) error
+}
+
+func mapSourceFile(m int) string       { return fmt.Sprintf("map_%d_source.sqlite3", m) }
+func mapInputFile(m int) string        { return fmt.Sprintf("map_%d_input.sqlite3", m) }
+func mapOutputFile(m, r int) string    { return fmt.Sprintf("map_%d_output_%d.sqlite3", m, r) }
+func reduceInputFile(r int) string     { return fmt.Sprintf("reduce_%d_input.sqlite3", r) }
+func reduceOutputFile(r int) string    { return fmt.Sprintf("reduce_%d_output.sqlite3", r) }
+func reducePartialFile(r int) string   { return fmt.Sprintf("reduce_%d_partial.sqlite3", r) }
+func reduceTempFile(r int) string      { return fmt.Sprintf("reduce_%d_temp.sqlite3", r) }
+func makeURL(host, file string) string { return fmt.Sprintf("http://%s/data/%s", host, file) }
+
+//func (task *MapTask) Process(tempdir string, client Interface) error{
+//
+//	//download and open input file
+//	download(SourceHost, //where do I get this?)
+//
+//	// create output files
+//
+//	// run a database query to select all pairs from the source file
+//
+//}
